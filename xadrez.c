1224de35d61e9f1de18b1916ce2451f5b24b3d9e
Bruno2Lord <146855007+Bruno746883@users.noreply.github.com>
2026-03-17 19:21:30 +0000

    Desafio nivel aventureiro feito e funcionando

--- xadrez.c
+++ xadrez.c
@@ -4,21 +4,20 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-      int i;
+    // Nível Aventureiro - Movimentação do Cavalo
+    int main() {
+
+    int i;
 
     /* ===== TORRE (for) ===== */
     printf("Movimento da Torre:\n");
-
     for(i = 1; i <= 5; i++) {
         printf("Direita\n");
     }
 
     /* ===== BISPO (while) ===== */
     printf("\nMovimento do Bispo:\n");
-
     int j = 1;
-
     while(j <= 5) {
         printf("Cima Direita\n");
         j++;
@@ -26,17 +25,29 @@ int main() {
 
     /* ===== RAINHA (do-while) ===== */
     printf("\nMovimento da Rainha:\n");
-
     int k = 1;
-
     do {
         printf("Esquerda\n");
         k++;
     } while(k <= 8);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    /* ===== CAVALO (loops aninhados) ===== */
+    printf("\nMovimento do Cavalo:\n");
+
+    int baixo = 2;
+    int esquerda = 1;
+
+    int x, y;
+
+    for(x = 1; x <= baixo; x++) {
+        printf("Baixo\n");
+    }
+
+    y = 1;
+    while(y <= esquerda) {
+        printf("Esquerda\n");
+        y++;
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
